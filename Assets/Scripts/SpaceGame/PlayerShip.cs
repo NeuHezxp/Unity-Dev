cdbab3cf007e34092674cf14742fa0033ee576ac
NeuHezxp <mpatino@student.neumont.edu>
2024-02-01 15:35:34 -0700

    Added First Weapon

---
+++ Assets/Scripts/SpaceGame/PlayerShip.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour
+{
+    [SerializeField] private Inventory inventory;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            inventory.Use();
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            inventory.stopUse();
+        }
+    }
+}
